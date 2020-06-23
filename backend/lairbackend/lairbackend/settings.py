9bf3d2be5116ef0078bfc83aa10dc7e584335174
= <luisfmanzanoa@gmail.com>
2020-06-23 14:45:02 -0400

    terminada la vista inicial y la configuracion basica de la API

--- backend/lairbackend/lairbackend/settings.py
+++ backend/lairbackend/lairbackend/settings.py
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
+    
     'Main',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +132,12 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = [
+    'http://google.com',
+    'http://hostname.example.com',
+    'http://localhost:3000',
+    'http://127.0.0.1:9000'
+]
--- frontend/lairfrontend/src/components/AdminPeliculas.js
+++ frontend/lairfrontend/src/components/AdminPeliculas.js
@@ -1,21 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 
 
 function AdminPeliculas(){
-
+//EL URL CON LAS PELICULAS (VA A CAMBIAR CUANDO COLOQUEMOS MAS TABLAS)
+//LA DIRECCION http://127.0.0.1:8000/api PUEDE CAMBIAR DEPENDIENDO DE SU COMPUTADORA
+//SI NO USA LA MISMA INTENTEN CONFIGURARLA PARA QUE TODOS USEMOS LA MISMA
     const url = `http://127.0.0.1:8000/api`
 
     const [movies, setMovies] = useState(null);
 
-    axios.get(url).then(res => {
+    //CON ESTE COMANDO DE AXIOS NOS TRAEMOS LO QUE NECESITAMOS DE LA API
+
+
+    useEffect(() => {
+      axios.get(url).then(res => {
         console.log(res.data);
-        this.setMovies(res.data);
+        setMovies(res.data);
+        console.log(movies)
     })
+    }, [url])
 
 
+    
+    if(movies!=null){
 
-    return(
+      return(
         <div className="container">
 
 <table className="table">
@@ -28,20 +38,21 @@ function AdminPeliculas(){
       <th scope="col">Genero</th>
       <th scope="col">Idioma</th>
       <th scope="col">Proyectando</th>
-      <th scope="col">Director</th>
     </tr>
   </thead>
   <tbody>
+   {movies.map(movie => (
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>Mark</td>
+      <th scope="row">{movie.id}</th>
+      <td>{movie.title}</td>
+      <td>{movie.duration}</td>
+      <td>{movie.director}</td>
+      <td>{movie.genre}</td>
+      <td>{movie.language}</td>
+      <td>{movie.showing ? 'Si' : '$No'}</td>
     </tr>
+  ))}
+
   </tbody>
 </table>
 
@@ -64,6 +75,17 @@ function AdminPeliculas(){
            
         </div>
     )
+    }
+
+    else {
+      return (
+        <h2>Cargando</h2>
+      )
+    }
+
+
+
+    
 }
 
 export default AdminPeliculas;
