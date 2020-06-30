72642c99026cfebd5c2653297220cabf676d55be
= <luisfmanzanoa@gmail.com>
2020-06-29 23:22:04 -0400

    creado VRUD completo

--- backend/lairbackend/Main/api/urls.py
+++ backend/lairbackend/Main/api/urls.py
@@ -1,9 +1,20 @@
 
 from django.urls import path
-from .views import MovieListView, MovieDetailView
+from .views import MovieViewSet
+from rest_framework.routers import DefaultRouter
+# from .views import MovieListView, MovieDetailView, MovieCreateView, MovieUpdateView
+
+router = DefaultRouter()
+router.register(r'movies', MovieViewSet, basename='movies')
+urlpatterns = router.urls
 
 
-urlpatterns = [
-     path('', MovieListView.as_view()),
-     path('<pk>', MovieDetailView.as_view())
-]
+# urlpatterns = [
+#      path('', MovieListView.as_view()),
+#      path('<pk>', MovieDetailView.as_view()),
+#      path('create/', MovieCreateView.as_view()),
+#      path('<pk>/update/', MovieCreateView.as_view()),
+# ]
+
+
+
--- backend/lairbackend/Main/api/views.py
+++ backend/lairbackend/Main/api/views.py
@@ -1,11 +1,31 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
 from Main.models import Movie
 from .serializers import MovieSerializer
+from rest_framework import viewsets
 
-class MovieListView(ListAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
 
-class MovieDetailView(RetrieveAPIView):
-    queryset = Movie.objects.all()
+class MovieViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
     serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
+
+
+
+
+# class MovieListView(ListAPIView):
+#     queryset = Movie.objects.all()
+#     serializer_class = MovieSerializer
+
+# class MovieDetailView(RetrieveAPIView):
+#     queryset = Movie.objects.all()
+#     serializer_class = MovieSerializer
+
+# class MovieCreateView(CreateAPIView):
+#     queryset = Movie.objects.all()
+#     serializer_class = MovieSerializer
+
+# class MovieUpdateView(UpdateAPIView):
+#     queryset = Movie.objects.all()
+#     serializer_class = MovieSerializer
