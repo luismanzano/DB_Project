368bc68fb5ac80a138f47f68f60c0a87ce0d1b5b
Reichel  Larez <Reichelpatchc@la-MacBook-Pro-de-Reichel.local>
2020-06-30 17:33:52 -0400

    cambios en home

--- frontend/lairfrontend/src/components/Home.jsx
+++ frontend/lairfrontend/src/components/Home.jsx
@@ -4,6 +4,10 @@ import './styles/Home.css'
 import Footer from './Footer'
 import popcorn1 from './images/popcorn1.png'
 import popcorn2 from './images/popcorn2.png'
+import comprar from './images/comprar.png'
+import trailer from './images/trailer.png'
+
+var imagen = "https://lumiere-a.akamaihd.net/v1/images/ba66734fc3379a905313db81d359b611_2737x4096_967d8263.jpeg"
 
 
 export default class Home extends Component{
@@ -33,20 +37,70 @@ export default class Home extends Component{
                                 <div row="row">
                                     <div className="col-md-12 gap-down">
                                         <div className="row">
-                                            <div className="col-md-2 offset-1">
-                                                <img className="pelicula" src="https://lumiere-a.akamaihd.net/v1/images/ba66734fc3379a905313db81d359b611_2737x4096_967d8263.jpeg" alt=""/>
+                                            <div className="col-md-2 offset-1 contenedor">
+                                                <img className="pelicula" src={imagen} alt=""/>                
+                                                <div className="links">
+                                                    <div className="row">
+                                                        <div className="col-md-4">
+                                                            <a href=""><img id="select" src={comprar} alt=""/></a>
+                                                        </div>
+                                                        <div className="col-md-4 offset-3">
+                                                            <a href=""><img id="select2" src={trailer} alt=""/></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className="col-md-2">
+                                            <div className="col-md-2 contenedor">
                                                 <img className="pelicula" src="https://es.web.img2.acsta.net/pictures/19/11/12/12/25/0815514.jpg" alt=""/>
+                                                <div className="links">
+                                                    <div className="row">
+                                                        <div className="col-md-4">
+                                                            <a href=""><img id="select" src={comprar} alt=""/></a>
+                                                        </div>
+                                                        <div className="col-md-4 offset-3">
+                                                            <a href=""><img id="select2" src={trailer} alt=""/></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className="col-md-2">
+                                            <div className="col-md-2 contenedor">
                                                 <img className="pelicula" src="https://es.web.img3.acsta.net/pictures/19/12/19/12/29/3194366.jpg" alt=""/>
+                                                <div className="links">
+                                                    <div className="row">
+                                                        <div className="col-md-4">
+                                                            <a href=""><img id="select" src={comprar} alt=""/></a>
+                                                        </div>
+                                                        <div className="col-md-4 offset-3">
+                                                            <a href=""><img id="select2" src={trailer} alt=""/></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className="col-md-2">
+                                            <div className="col-md-2 contenedor">
                                                 <img className="pelicula" src="https://es.web.img2.acsta.net/pictures/20/01/22/12/46/2447825.jpg" alt=""/>
+                                                <div className="links">
+                                                    <div className="row">
+                                                        <div className="col-md-4">
+                                                            <a href=""><img id="select" src={comprar} alt=""/></a>
+                                                        </div>
+                                                        <div className="col-md-4 offset-3">
+                                                            <a href=""><img id="select2" src={trailer} alt=""/></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className="col-md-2">
+                                            <div className="col-md-2 contenedor">
                                                 <img className="pelicula" src="https://es.web.img2.acsta.net/pictures/19/11/20/17/14/1219132.jpg" alt=""/>
+                                                <div className="links">
+                                                    <div className="row">
+                                                        <div className="col-md-4">
+                                                            <a href=""><img id="select" src={comprar} alt=""/></a>
+                                                        </div>
+                                                        <div className="col-md-4 offset-3">
+                                                            <a href=""><img id="select2" src={trailer} alt=""/></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -54,7 +108,7 @@ export default class Home extends Component{
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-md-10 offset-1 gap-up fondoMorado gap-down">
+                            <div className="col-md-10 offset-1 gap-up fondoMorado gap-down secretCombo">
                                 <div className="row">
                                     <div className="col-md-4 imagen">
                                         <img id="popcorn" src={popcorn1} alt=""/>
--- frontend/lairfrontend/src/components/index.jsx
+++ frontend/lairfrontend/src/components/index.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Home from './Home'
+import AdminPeliculas from './AdminPeliculas'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
