c467f47847cf82b3db72a486ff9bbd0ea8cccd79
Reichel  Larez <Reichelpatchc@la-MacBook-Pro-de-Reichel.local>
2020-06-30 22:29:09 -0400

    Vistas Clientes

---
+++ frontend/lairfrontend/src/components/Carrito.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Carrito = ()=>(
+    <div className="container">
+        <div className="row">
+            <div className="col-md-10 offset-1 gap-up gap-down">
+                Aqui van los articulos
+
+                <div className="row">
+                    <div className="col-md-3 offset-9 text-center gap-up gap-down">
+                        <button className="boton">Pagar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+)
+
+export default Carrito
