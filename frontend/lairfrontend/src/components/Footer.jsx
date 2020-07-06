105ad99950393a90ef69949db63e3e9682175821
Miguelangel S <miguelants14@gmail.com>
2020-07-05 20:42:23 -0400

    Footer

--- frontend/lairfrontend/src/components/Footer.jsx
+++ frontend/lairfrontend/src/components/Footer.jsx
@@ -11,12 +11,12 @@ export default class Footer extends Component {
                 <div className="col-md-10 fondoGris offset-1 gap-up gap-down">
 
                     <div className="row">
-                        <div className="col-md-3">
+                        <div className="col-md-4">
                         <a href=""><img id="logo" src={instagram} width="50" height="50" /></a>
                             <a href=""><img id="logo" src={facebook} width="50" height="50" /></a>
                             <a href=""><img id="logo" src={twitter} width="50" height="50" /></a>
                         </div>
-                        <div className="col-md-6"></div>
+                        <div className="col-md-5"></div>
                         <div className="col-md-3">
                             <br/>
                             <h3 className="placa"><b>The Lair 2020 ™</b></h3>
