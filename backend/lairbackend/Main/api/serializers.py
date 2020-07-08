42d7e23ee1e3bcc3bb3f6d38f1d272e6893e1caf
= <luisfmanzanoa@gmail.com>
2020-07-08 14:48:11 -0400

    anadido el retrieve de ID

--- backend/lairbackend/Main/api/serializers.py
+++ backend/lairbackend/Main/api/serializers.py
@@ -14,13 +14,13 @@ AsientosOcupados
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('title', 'duration', 'reparto', 'showing', 'director', 'genre', 'language', 'synopsis', 'thumbnail')
+        fields = ('id','title', 'duration', 'reparto', 'showing', 'director', 'genre', 'language', 'synopsis', 'thumbnail')
 
 
 class EntradasSerializer(serializers.ModelSerializer):
     class Meta:
         model = Entradas
-        fields = ('serial', 'costo', 'id_productos', 'id_proyecciones')
+        fields = ('id', 'serial', 'costo', 'id_productos', 'id_proyecciones')
 
 
 class ProductoSerializer(serializers.ModelSerializer):
@@ -43,25 +43,25 @@ class SalasSerializer(serializers.ModelSerializer):
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
-        fields = ('cedula', 'nombre', 'email', 'estado', 'nacimiento', 'apellido')
+        fields = ('id', 'cedula', 'nombre', 'email', 'estado', 'nacimiento', 'apellido')
 
 
 class VentaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Venta
-        fields = ('serial', 'fecha', 'id_cliente', 'monto_total')
+        fields = ('id', 'serial', 'fecha', 'id_cliente', 'monto_total')
 
 
 class AlimentosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Alimentos
-        fields = ('nombre', 'costo', 'cantidad', 'categoria', 'caducidad', 'id_producto')
+        fields = ('id', 'nombre', 'costo', 'cantidad', 'categoria', 'caducidad', 'id_producto')
 
 
 class AsientosOcupadosSerializer(serializers.ModelSerializer):
     class Meta:
         model = AsientosOcupados
-        fields = ('columna', 'fila', 'id_funciones')
+        fields = ('id', 'columna', 'fila', 'id_funciones')
 
 # class CarteleraSerializer(serializers.ModelSerializer):
 #     class Meta:
