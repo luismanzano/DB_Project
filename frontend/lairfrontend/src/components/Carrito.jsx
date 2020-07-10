47fecc5581b1b87984da4624dac0ecf4b649edf9
= <luisfmanzanoa@gmail.com>
2020-07-10 14:18:57 -0400

    ta carrito funciona

--- frontend/lairfrontend/src/components/Carrito.jsx
+++ frontend/lairfrontend/src/components/Carrito.jsx
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 function Carrito(){
 
+    const [re, setRe] = useState(null)
+
     function pay(carrito, montoTotal, e){
         e.preventDefault();
 
@@ -54,6 +56,46 @@ function Carrito(){
 
     }
 
+
+
+
+    function delete1(item, e){
+        e.preventDefault();
+
+        console.log('re', re)
+
+        console.log('item Enviado', item);
+        for(let i = 0; i<carrito.length; i++){
+            if(carrito[i].id == item.id){
+                console.log('carrito a cambiar', carrito[i])
+                if(carrito[i].cantidad > 1)
+                carrito[i].cantidad--;
+
+                else if(carrito[i].cantidad == 1){
+                    carrito.splice(i, 1)
+                }
+                
+                console.log('carrito cambiado', carrito);
+
+                localStorage.setItem('carrito', JSON.stringify(carrito))
+                console.log('carritoLocal', localStorage.getItem('carrito'))
+                
+                setRe(carrito)
+                console.log('re2', re)
+
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
     const getAlimentos = `http://127.0.0.1:8000/api/alimentos/`
 
 
@@ -86,7 +128,7 @@ else if (carrito != null && carrito != ''){
                 <div className="row">
                     <div className="col-md-10 offset-1 gap-up gap-down">
                     <div className='row white-font gap-down border-bottom some-padding'>
-                            <div className='col-md-3'>Foto</div>
+                            <div className='col-md-2'>Foto</div>
                             <div className='col-md-3'>Nombre</div>
                             <div className='col-md-2'>Cantidad</div>
                             <div className='col-md-3'>Monto</div>
@@ -97,10 +139,13 @@ else if (carrito != null && carrito != ''){
                          {
                             carrito.map(carr => (
                             <div className='row white-font gap-down border-bottom some-padding'>
-                            <div className='col-md-3'>Foto</div>
+                            <div className='col-md-2'>Foto</div>
                             <div className='col-md-3'>{carr.nombre}</div>
                             <div className='col-md-2'>{carr.cantidad}</div>
                             <div className='col-md-3'>{carr.cantidad * carr.costo}</div>
+                            <div className='col-md-2'>
+                                <div className='btn btn-danger' onClick={(e) => delete1(carr, e)}>Eliminar 1</div>
+                            </div>
                         </div>
                             ))
                         }
