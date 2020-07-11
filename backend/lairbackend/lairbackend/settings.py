901e20f53acdd60372742a6c41f22346e851f0b2
Reichel  Larez <Reichelpatchc@la-MacBook-Pro-de-Reichel.local>
2020-07-11 01:12:22 -0400

    cambios iniciales

--- backend/lairbackend/lairbackend/settings.py
+++ backend/lairbackend/lairbackend/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 5,
     
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 
 
